3e568579fed3d96ba91c54ce23daca68896ee43c
HSKGecko <support@hskgecko.com>
2023-03-29 21:28:50 +0100

    using visible attribute

---
+++ cypress/e2e/basicAsserctions.cy.js
@@ -0,0 +1,12 @@
+
+describe('My first test suite', ()=>{
+
+    it('My First Test Case', () => {
+
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type('ca')
+        cy.wait(2000)
+
+        cy.get('.product:visible').should('have.length', 4)
+    })
+})
