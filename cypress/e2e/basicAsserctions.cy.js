e1769b562cf336e2810de6faf25c248d878add83
HSKGecko <support@hskgecko.com>
2023-03-29 21:49:58 +0100

    using eq, contains and click

--- cypress/e2e/basicAsserctions.cy.js
+++ cypress/e2e/basicAsserctions.cy.js
@@ -8,5 +8,7 @@ describe('My first test suite', ()=>{
         cy.wait(2000)
 
         cy.get('.product:visible').should('have.length', 4)
+        cy.get('.products').find('.product').should('have.length', 4)
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
     })
 })
